3caf21f947af1d30a075b4819e461e68e5ed9240
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2019-10-08 11:57:35 -0700

    move components into correct folder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-import Header from '../components/Header';
-import About from '../components/About';
-import Home from '../components/Home';
-import Portfolio from '../components/Portfolio';
-import Resume from '../components/Resume';
-import Footer from '../components/Footer';
+import Header from './components/Header';
+import About from './components/About';
+import Home from './components/Home';
+import Portfolio from './components/Portfolio';
+import Resume from './components/Resume';
+import Footer from './components/Footer';
 
 import { HashRouter as Router, Route } from 'react-router-dom';
 import './App.css';
