11e2d4f8eafff85778098743aea53a9b004fe9cd
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2019-12-13 16:41:01 -0800

    upgrade files

---
+++ src/resumeData.js
@@ -0,0 +1,89 @@
+let resumeData = {
+  "imagebaseurl":"https://rbhatia46.github.io/",
+  "name": "Stephanie Cherubin",
+  "role": "Frontend Developer & Full Stack",
+  "linkedinId":"Your LinkedIn Id",
+  "twitterid": "Your twitterid",
+  "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or make Javascript games in my free time.",
+  "socialLinks":[
+      {
+        "name":"linkedin",
+        "url":"https://www.linkedin.com/in/stephanie-cherubin/",
+        "className":"fa fa-linkedin"
+      },
+      {
+        "name":"github",
+        "url":"https://github.com/StephanieCherubin/",
+        "className":"fa fa-github"
+      },
+      {
+        "name":"twitter",
+        "url":"http://twitter.com/rbhatia46",
+        "className":"fa fa-twitter"
+      }
+    ],
+  "aboutme":"I am currently a student at Make School pursuing my Bachelor's Degree in Science. I am a Full Stack Web Developer. Results-oriented, driven professional with a proven track record of demonstrating excellence. I am a lifelong learner currently taking classes to further my professional development. #NeverStopLearning Check out my Plum results here: app.plum.io/profile/80ff2cf4d5f9bb72bcdf741341655ffeb6c81b6b     ",
+  "address":"...",
+  "website":"",
+  "education":[
+    {
+      "UniversityName":"Make School",
+      "specialization":"Front end Development",
+      "MonthOfPassing":"Aug",
+      "YearOfPassing":"2020",
+      "Achievements":"Some Achievements"
+    },
+  ],
+  "work":[
+    {
+      "CompanyName":"Black Girls Code",
+      "specialization":"Teaching computer programming",
+      "MonthOfLeaving":"August",
+      "YearOfLeaving":"2019",
+      "Achievements":"Some Achievements"
+    },
+    {
+      "CompanyName":"Octopi",
+      "specialization":"Software Engineering",
+      "MonthOfLeaving":"Jan",
+      "YearOfLeaving":"2018",
+      "Achievements":"Some Achievements"
+    }
+  ],
+  "skillsDescription":"Your skills here",
+  "skills":[
+    {
+      "skillname":"HTML5"
+    },
+    {
+      "skillname":"CSS"
+    },
+    {
+      "skillname":"Reactjs"
+    }
+  ],
+  "portfolio":[
+    {
+      "name":"project1",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/phone.jpg"
+    },
+    {
+      "name":"project2",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/project.jpg"
+    },
+    {
+      "name":"project3",
+      "description":"mobileapp",  
+      "imgurl":"images/portfolio/project2.png"
+    },
+    {
+      "name":"project4",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/phone.jpg"
+    }
+  ],
+}
+
+export default resumeData
